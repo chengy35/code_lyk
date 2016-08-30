e4f18b507c92c8cc199a859069b10aac065af64a
chengy35 <chengy35@mail2.sysu.edu.cn>
2016-08-30 15:26:09 +0800

    word

--- kernel/KHistogram/DenseTrack.cpp
+++ kernel/KHistogram/DenseTrack.cpp
@@ -48,186 +48,51 @@ bool DenseTrack(int argc, char** argv)
 			printf("break");
 			break;
 		}
-		if( frameNum >= start_frame && frameNum <= end_frame ) {
-		if( !image ) {
-			// initailize all the buffers
-			image = IplImageWrapper( cvGetSize(frame), 8, 3 );
-			image->origin = frame->origin;
-			prev_image= IplImageWrapper( cvGetSize(frame), 8, 3 );
-			prev_image->origin = frame->origin;
-			grey = IplImageWrapper( cvGetSize(frame), 8, 1 );
-			grey_pyramid = IplImagePyramid( cvGetSize(frame), 8, 1, scale_stride );
-			prev_grey = IplImageWrapper( cvGetSize(frame), 8, 1 );
-			prev_grey_pyramid = IplImagePyramid( cvGetSize(frame), 8, 1, scale_stride );
-			eig_pyramid = IplImagePyramid( cvGetSize(frame), 32, 1, scale_stride );
-
-			cvCopy( frame, image, 0 );
-			cvCvtColor( image, grey, CV_BGR2GRAY );
-			grey_pyramid.rebuild( grey );
-
-			// how many scale we can have
-			scale_num = std::min<std::size_t>(scale_num, grey_pyramid.numOfLevels());
-			fscales = (float*)cvAlloc(scale_num*sizeof(float));
-			xyScaleTracks.resize(scale_num);
-
-			for( int ixyScale = 0; ixyScale < scale_num; ++ixyScale ) {
-				std::list<Track>& tracks = xyScaleTracks[ixyScale];
-				fscales[ixyScale] = pow(scale_stride, ixyScale);
-
-				// find good features at each scale separately
-				IplImage *grey_temp = 0, *eig_temp = 0;
-				std::size_t temp_level = (std::size_t)ixyScale;
-				grey_temp = cvCloneImage(grey_pyramid.getImage(temp_level));
-				eig_temp = cvCloneImage(eig_pyramid.getImage(temp_level));
-				std::vector<CvPoint2D32f> points(0);
-				cvDenseSample(grey_temp, eig_temp, points, quality, min_distance);
-				
-				// save the feature points
-				for( i = 0; i < points.size(); i++ ) {
-					Track track(tracker.trackLength);
-					PointDesc point(hogInfo, hofInfo, mbhInfo, points[i], 0, 0);
-					track.addPointDesc(point);
-					tracks.push_back(track);
-				}
-
-				cvReleaseImage( &grey_temp );
-				cvReleaseImage( &eig_temp );
-			}
-		}
+		if( frameNum >= start_frame && frameNum <= end_frame )
+		{
+			
 
 		// build the image pyramid for the current frame
 		cvCopy( frame, image, 0 );
 		cvCvtColor( image, grey, CV_BGR2GRAY );
 		grey_pyramid.rebuild(grey);
 
-		if( frameNum > 0 ) {
-		init_counter++;
-		for( int ixyScale = 0; ixyScale < scale_num; ++ixyScale ) {
-			// track feature points in each scale separately
-			std::vector<CvPoint2D32f> points_in(0);
-			std::list<Track>& tracks = xyScaleTracks[ixyScale];
-			for (std::list<Track>::iterator iTrack = tracks.begin(); iTrack != tracks.end(); ++iTrack) {
-				CvPoint2D32f point = iTrack->pointDescs.back().point;
-				points_in.push_back(point); // collect all the feature points
-			}
-			int count = points_in.size();
-			IplImage *prev_grey_temp = 0, *grey_temp = 0;
-			std::size_t temp_level = ixyScale;
-			prev_grey_temp = cvCloneImage(prev_grey_pyramid.getImage(temp_level));
-			grey_temp = cvCloneImage(grey_pyramid.getImage(temp_level));
+		if( frameNum > 0 ) 
+		{
+			init_counter++;
+			for( int ixyScale = 0; ixyScale < scale_num; ++ixyScale )
+			{
+				
+				
 
-			cv::Mat prev_grey_mat = cv::cvarrToMat(prev_grey_temp);
-			cv::Mat grey_mat = cv::cvarrToMat(grey_temp);
+				i = 0;
+				for (std::list<Track>::iterator iTrack = tracks.begin(); iTrack != tracks.end(); ++i)
+				{
+					if( status[i] == 1 ) { // if the feature point is successfully tracked
+						PointDesc& pointDesc = iTrack->pointDescs.back();
+						CvPoint2D32f prev_point = points_in[i];
+						// get the descriptors for the feature point
+						CvScalar rect = getRect(prev_point, cvSize(width, height), hogInfo);
+						pointDesc.hog = getDesc(hogMat, rect, hogInfo);
+						pointDesc.hof = getDesc(hofMat, rect, hofInfo);
+						pointDesc.mbhX = getDesc(mbhMatX, rect, mbhInfo);
+						pointDesc.mbhY = getDesc(mbhMatY, rect, mbhInfo);
 
-
-			// compute the optical flow
-			IplImage* flow = cvCreateImage(cvGetSize(grey_temp), IPL_DEPTH_32F, 2);
-			cv::Mat flow_mat = cv::cvarrToMat(flow);
-			cv::calcOpticalFlowFarneback( prev_grey_mat, grey_mat, flow_mat,
-							sqrt(2)/2.0, 5, 10, 2, 7, 1.5, cv::OPTFLOW_FARNEBACK_GAUSSIAN );
-			// prevImg(y, x) = nextImg(y + flow(y, x)[1], x + flow(y, x)[0])?
-			// the point (y,x) of previous image move to (y + flow(y, x)[1], x + flow(y, x)[0]) of next image
-
-			int width = grey_temp->width;
-			int height = grey_temp->height;
-			//--------------------------------begin--------------------------------
-			//-------------------------modified by Yikun Lin-----------------------
-			// compute the integral histograms
-			DescMat* hogMat = InitDescMat(height, width, hogInfo.nBins);
-			HogComp(prev_grey_temp, hogMat, hogInfo, kernelMatrix);
-
-			DescMat* hofMat = InitDescMat(height, width, hofInfo.nBins);
-			HofComp(flow, hofMat, hofInfo, kernelMatrix);
-
-			DescMat* mbhMatX = InitDescMat(height, width, mbhInfo.nBins);
-			DescMat* mbhMatY = InitDescMat(height, width, mbhInfo.nBins);
-			MbhComp(flow, mbhMatX, mbhMatY, mbhInfo, kernelMatrix);
-			
-			// calculate static saliency map
-			CvMat* staticSalMap = cvCreateMat(height, width, CV_32FC1);
-			float staticSaliency = sal.CalcStaticSaliencyMap(prev_grey_temp, staticSalMap, true);
-			if( show_track == 1 ) {
-				IplImage* pImg = cvCreateImage(cvGetSize(staticSalMap), IPL_DEPTH_32F, 1);
-				cvConvert(staticSalMap, pImg);
-				char str[20];
-				sprintf(str, "saliency1/%d.jpg", frameNum);
-				if (ixyScale == 0)
-					cvSaveImage(str, pImg);
-				cvReleaseImage( &pImg );
-			}
-			
-			// calculate dynamic saliency map
-			CvMat* dynamicSalMap = cvCreateMat(height, width, CV_32FC1);
-			float dynamicSaliency = sal.CalcMotionSaliencyMap(flow, hofInfo, kernelMatrix, dynamicSalMap, true);
-			if( show_track == 1 ) {
-				IplImage* pImg = cvCreateImage(cvGetSize(dynamicSalMap), IPL_DEPTH_32F, 1);
-				cvConvert(dynamicSalMap, pImg);
-				char str[20];
-				sprintf(str, "saliency2/%d.jpg", frameNum);
-				if (ixyScale == 0)
-					cvSaveImage(str, pImg);
-				cvReleaseImage( &pImg );
-			}
-			
-			// calculate combined saliency map
-			CvMat* salMap = cvCreateMat(height, width, CV_32FC1);
-			for(int iHeight = 0; iHeight < height; iHeight++) {
-				float* pMap = (float*)(salMap->data.ptr + iHeight * salMap->step);
-				float* pStaticMap = (float*)(staticSalMap->data.ptr + iHeight * staticSalMap->step);
-				float* pDynamicMap = (float*)(dynamicSalMap->data.ptr + iHeight * dynamicSalMap->step);
-				for(int iWidth = 0; iWidth < width; iWidth++) {
-					pMap[iWidth] = staticRatio * pStaticMap[iWidth] + dynamicRatio * pDynamicMap[iWidth];
+						PointDesc point(hogInfo, hofInfo, mbhInfo, points_out[i], saliency[i], averageSaliency);
+						iTrack->addPointDesc(point);	
+						++iTrack;
+					}
+					else // remove the track, if we lose feature point
+					iTrack = tracks.erase(iTrack);
 				}
+				ReleDescMat(hogMat);
+				ReleDescMat(hofMat);
+				ReleDescMat(mbhMatX);
+				ReleDescMat(mbhMatY);
+				cvReleaseImage( &prev_grey_temp );
+				cvReleaseImage( &grey_temp );
+				cvReleaseImage( &flow );
 			}
-			if( show_track == 1 ) {
-				IplImage* pImg = cvCreateImage(cvGetSize(salMap), IPL_DEPTH_32F, 1);
-				cvConvert(salMap, pImg);
-				char str[20];
-				sprintf(str, "saliency3/%d.jpg", frameNum);
-				if (ixyScale == 0)
-					cvSaveImage(str, pImg);
-				cvReleaseImage( &pImg );
-			}
-			float averageSaliency = staticRatio * staticSaliency + dynamicRatio * dynamicSaliency;
-			std::vector<int> status(count);
-			std::vector<CvPoint2D32f> points_out(count);
-			std::vector<float> saliency(count);
-
-			// track feature points by median filtering
-			OpticalFlowTracker(flow, salMap, points_in, points_out, status, saliency);
-			
-			cvReleaseMat(&salMap);
-			cvReleaseMat(&dynamicSalMap);
-			cvReleaseMat(&staticSalMap);
-			//---------------------------------end---------------------------
-
-			i = 0;
-			for (std::list<Track>::iterator iTrack = tracks.begin(); iTrack != tracks.end(); ++i) {
-			if( status[i] == 1 ) { // if the feature point is successfully tracked
-				PointDesc& pointDesc = iTrack->pointDescs.back();
-				CvPoint2D32f prev_point = points_in[i];
-				// get the descriptors for the feature point
-				CvScalar rect = getRect(prev_point, cvSize(width, height), hogInfo);
-				pointDesc.hog = getDesc(hogMat, rect, hogInfo);
-				pointDesc.hof = getDesc(hofMat, rect, hofInfo);
-				pointDesc.mbhX = getDesc(mbhMatX, rect, mbhInfo);
-				pointDesc.mbhY = getDesc(mbhMatY, rect, mbhInfo);
-
-				PointDesc point(hogInfo, hofInfo, mbhInfo, points_out[i], saliency[i], averageSaliency);
-				iTrack->addPointDesc(point);	
-				++iTrack;
-			}
-			else // remove the track, if we lose feature point
-				iTrack = tracks.erase(iTrack);
-			}
-			ReleDescMat(hogMat);
-			ReleDescMat(hofMat);
-			ReleDescMat(mbhMatX);
-			ReleDescMat(mbhMatY);
-			cvReleaseImage( &prev_grey_temp );
-			cvReleaseImage( &grey_temp );
-			cvReleaseImage( &flow );
-		}
 
 		for( int ixyScale = 0; ixyScale < scale_num; ++ixyScale ) {
 		std::list<Track>& tracks = xyScaleTracks[ixyScale]; // output the features for each scale
